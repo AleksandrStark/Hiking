c94768e739a4230b8342cf16f40e49b40c999000
Aleksandr Starkov <77936849+AleksandrStark@users.noreply.github.com>
2021-11-29 22:46:50 +0300

    commit

---
+++ src/Components/FormAdd.js
@@ -0,0 +1,48 @@
+import HikingModel from './HikingModel';
+import {nanoid} from 'nanoid';
+import {useState} from 'react';
+
+export default function FormAdd({ inputValue }) {
+    const [dataInput, setData] = useState({
+        date: '',
+        distance: ''
+    });
+
+    const handleSubmit = evt => {
+        evt.preventDefault();          
+        const values = new HikingModel(nanoid(), dataInput.date, dataInput.distance);
+        inputValue(values);
+        setData(prevForm => ({...prevForm, date: '', distance: ''}));
+        
+    };
+
+    const handleDataChange = evt => {     
+        const value = evt.target.value;
+        const name = evt.target.name;
+        setData(prevForm => ({...prevForm, [name]:value}));
+    };
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="date">Дата (ДД.ММ.ГГ)</label>
+                <input 
+                    id="date" 
+                    type="date" 
+                    name="date"
+                    value={dataInput.date}
+                    onChange={handleDataChange} />
+            </div>
+            <div>
+                <label htmlFor="distance">Пройдено км</label>
+                <input 
+                    id="distance" 
+                    type="text" 
+                    name="distance" 
+                    value={dataInput.distance}
+                    onChange={handleDataChange} />
+            </div>
+            <button type="submit">OK</button>
+        </form>
+    );
+}
---
+++ src/Components/HikingModel.js
@@ -0,0 +1,9 @@
+class HikingModel {
+    constructor(id, date, distance) {
+        this.id = id;
+        this.date = date;
+        this.distance = distance;
+    }
+}
+
+export default HikingModel;
